c86af41b70440e3bf41d5d24094e80fbbac0b8c6
Odilbek <113532560+odilbekl@users.noreply.github.com>
2022-11-06 18:51:47 +0500

    site end

---
+++ javascript/main.js
@@ -0,0 +1,6 @@
+var btn = document.querySelector('.site-header__menu-box-mobile');
+var list = document.querySelector('.site-header__button');
+
+btn.addEventListener('click',function(){
+  list.style.display = 'flex';
+})
